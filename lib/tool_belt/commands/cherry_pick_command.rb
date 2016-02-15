8c1044700e8d9b70467d473e88ddcc72aaae6e63
Chris Peters <chrispeters.psu@gmail.com>
2016-02-15 16:06:42 -0500

    Updating cherry_pick_command to allow for bugzilla
    (cherry picked from commit b4c5384f46a34579f39ae4872cf7e56a74de27c8)

--- lib/tool_belt/commands/cherry_pick_command.rb
+++ lib/tool_belt/commands/cherry_pick_command.rb
@@ -3,11 +3,12 @@ module ToolBelt
     class CherryPickCommand < Clamp::Command
 
       parameter "config_file", "Release configuration file"
+      option "--bugzilla", :flag, "Bugzilla access"
       option "--update-cache", :flag, "Re-download issues and cache them"
       option "--version", 'VERSION', "Version to generate changelog for"
 
       def execute
-        config = ToolBelt::Config.new(config_file, version)
+        config = ToolBelt::Config.new(config_file, version, bugzilla?)
 
         issue_cache = ToolBelt::IssueCache.new(config.options)
         issues = issue_cache.load_issues(update_cache?)
