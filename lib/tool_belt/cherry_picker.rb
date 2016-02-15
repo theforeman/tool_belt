5fd1a89eca127b3c663aaef3fc076154b35da035
Chris Peters <chrispeters.psu@gmail.com>
2016-02-15 16:49:02 -0500

    Add check for refs commit message in cherry-picker

--- lib/tool_belt/cherry_picker.rb
+++ lib/tool_belt/cherry_picker.rb
@@ -27,7 +27,7 @@ module ToolBelt
         commits = issue['changesets']
 
         commits.each do |commit|
-          if commit['comments'].downcase.start_with?('fixes') && !@release_environment.commit_in_release_branch?(repo_names, commit['comments'])
+          if commit['comments'].downcase.start_with?('fixes', 'refs') && !@release_environment.commit_in_release_branch?(repo_names, commit['comments'])
             revisions << commit['revision']
           end
         end
