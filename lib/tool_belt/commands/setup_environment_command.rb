e3abe945efbcb0f694c489184d7fc7fc2074bc27
Chris Peters <chrispeters.psu@gmail.com>
2016-02-15 16:07:06 -0500

    Updating setup_environment_command to allow for bugzilla
    (cherry picked from commit de226c0322a7c7a12a252cf1e9ceddaff3f4db40)

--- lib/tool_belt/commands/setup_environment_command.rb
+++ lib/tool_belt/commands/setup_environment_command.rb
@@ -3,10 +3,11 @@ module ToolBelt
     class SetupEnvironmentCommand < Clamp::Command
 
       parameter "config_file", "Release configuration file"
+      option "--bugzilla", :flag, "Setup environment with Bugzilla access"
       option "--github-username", "USERNAME", "Add users forks for each repository that is setup, value must be the username on github"
 
       def execute
-        config = ToolBelt::Config.new(config_file)
+        config = ToolBelt::Config.new(config_file, nil, bugzilla?)
         release_environment = ToolBelt::ReleaseEnvironment.new(config.options.repos, config.options.namespace)
         release_environment.setup(:github_username => github_username)
       end
