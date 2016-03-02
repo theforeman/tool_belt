14a1f622121656df2349223c82fc48f4988b623e
Chris Peters <chrispeters.psu@gmail.com>
2016-03-02 11:46:38 -0500

    Fetch upstream remote after adding to release environment

--- lib/tool_belt/release_environment.rb
+++ lib/tool_belt/release_environment.rb
@@ -37,6 +37,7 @@ module ToolBelt
             if repo[:upstream]
               output, _success = @systools.execute("git remote show | grep upstream")
               @systools.execute("git remote add upstream #{repo[:upstream]}") unless output
+              @systools.execute("git fetch upstream")
             end
           end
         end
