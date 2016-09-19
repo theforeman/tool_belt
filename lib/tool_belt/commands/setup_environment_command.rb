3ba5e67ee1ae32be26f2e976f67a84498b64e8e9
Justin Sherrill <jlsherrill@gmail.com>
2016-09-19 12:42:24 -0400

    add version option to setup environment (#33)

--- lib/tool_belt/commands/setup_environment_command.rb
+++ lib/tool_belt/commands/setup_environment_command.rb
@@ -4,9 +4,10 @@ module ToolBelt
 
       parameter "config_file", "Release configuration file"
       option "--github-username", "USERNAME", "Add users forks for each repository that is setup, value must be the username on github"
+      option "--version", 'VERSION', "Version to build for"
 
       def execute
-        config = ToolBelt::Config.new(config_file)
+        config = ToolBelt::Config.new(config_file, version)
         release_environment = ToolBelt::ReleaseEnvironment.new(config.options.repos, config.options.namespace)
         release_environment.setup(:github_username => github_username)
       end
