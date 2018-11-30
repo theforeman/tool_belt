d412d88a31f153a23e664be3d80d10f07894d701
Eric Helms <eric.d.helms@gmail.com>
2018-11-30 09:15:22 -0500

    Add Katello 3.10 (#129)
    * Add Katello 3.10
    
    * Make procedures more generic to support other projects
    
    * Add strict_keys configuration
    
    * Drop puppet-service_wait

--- lib/tool_belt/commands/procedure.rb
+++ lib/tool_belt/commands/procedure.rb
@@ -1,61 +1,61 @@
 module ToolBelt
   module Command
     class ProcedureCommand < Clamp::Command
-      subcommand "foreman", "Foreman procedures" do
-        subcommand "branch", "Start branching Foreman" do
-          parameter "release", "Version that's currently in nightly in the major.minor format, like 1.20." do |value|
-            raise ArgumentError.new('Release must be in major.minor, like 1.20') unless value =~ /^\d+\.\d+$/
-            value
-          end
-
-          def execute
-            context = {
-              release: release,
-              develop: bump_last(release),
-            }
-
-            render('branch', context)
-          end
+      subcommand "branch", "Branch procedure for project" do
+        parameter "project", "Project to generate procedure for"
+        parameter "release", "Version that's currently in nightly in the major.minor format, like 1.20." do |value|
+          raise ArgumentError.new('Release must be in major.minor, like 1.20') unless value =~ /^\d+\.\d+$/
+          value
         end
 
-        subcommand "release", "Start releasing Foreman" do
-          parameter "full-version", "Version that the branch will have - like 1.20.0-RC1" do |value|
-            raise ArgumentError.new('Release must be in major.minor.patch or major.minor.patch-RCx, like 1.20.0 or 1.20.0-RC1') unless value =~ /^\d+\.\d+\.\d+(-RC\d+)?$/
-            value
-          end
+        def execute
+          context = {
+            release: release,
+            develop: bump_last(release),
+          }
 
-          def execute
-            version, extra = full_version.split('-', 2)
-            major, minor, _ = version.split('.', 3)
-            debian_full_version = version + (extra ? "~#{extra.downcase}" : '') + '-1'
-            context = {
-              is_rc: !extra.nil?,
-              extra: extra,
-              branch: "#{major}.#{minor}", # 1.20
-              debian_full_version: debian_full_version, # 1.20.0~rc1-1
-              full_version: full_version, # 1.20.0-RC1
-              version: version, # 1.20.0
-            }
+          render(project, 'branch', context)
+        end
+      end
 
-            render('release', context)
-          end
+      subcommand "release", "Release procedures for project" do
+        parameter "project", "Project to generate procedure for"
+        parameter "full-version", "Version that the branch will have - like 1.20.0-RC1" do |value|
+          raise ArgumentError.new('Release must be in major.minor.patch or major.minor.patch-RCx, like 1.20.0 or 1.20.0-RC1') unless value =~ /^\d+\.\d+\.\d+(-RC\d+)?$/
+          value
         end
 
-        private
+        def execute
+          version, extra = full_version.split('-', 2)
+          major, minor, _ = version.split('.', 3)
+          debian_full_version = version + (extra ? "~#{extra.downcase}" : '') + '-1'
+          context = {
+            is_rc: !extra.nil?,
+            extra: extra,
+            branch: "#{major}.#{minor}", # 1.20
+            debian_full_version: debian_full_version, # 1.20.0~rc1-1
+            full_version: full_version, # 1.20.0-RC1
+            version: version, # 1.20.0
+          }
 
-        def render(procedure, context)
-          filename = procedure_filename(procedure)
-          puts ToolBelt::Template.render_file(filename, context)
+          render(project, 'release', context)
         end
+      end
 
-        def procedure_filename(procedure)
-          "procedures/foreman/#{procedure}.md.erb"
-        end
+      private
 
-        def bump_last(version)
-          parts = version.split('.')
-          (parts[0..-2] + [(parts.last.to_i + 1).to_s]).join('.')
-        end
+      def render(project, procedure, context)
+        filename = procedure_filename(project, procedure)
+        puts ToolBelt::Template.render_file(filename, context)
+      end
+
+      def procedure_filename(project, procedure)
+        "procedures/#{project}/#{procedure}.md.erb"
+      end
+
+      def bump_last(version)
+        parts = version.split('.')
+        (parts[0..-2] + [(parts.last.to_i + 1).to_s]).join('.')
       end
     end
   end
--- lib/tool_belt/mash_scripts_builder.rb
+++ lib/tool_belt/mash_scripts_builder.rb
@@ -2,11 +2,12 @@ require 'fileutils'
 
 module ToolBelt
   class MashScriptsBuilder
-    attr_accessor :path, :tags, :gpgkey
+    attr_accessor :path, :tags, :gpgkey, :strict_keys
 
     def initialize(config)
       self.tags = config.options[:mash_scripts]
       self.gpgkey = config.options[:gpg_key]
+      self.strict_keys = config.options[:strict_keys] ? 'True' : 'False'
 
       self.path = "./mash_scripts/#{config.options[:namespace]}"
       FileUtils.mkpath(self.path)
@@ -15,7 +16,7 @@ module ToolBelt
     def create_mash_scripts
       self.tags.each do |tag|
         filename = File.join(self.path, "#{tag}.mash")
-        script = MashScript.new(filename, :tag => tag, :gpgkey => self.gpgkey)
+        script = MashScript.new(filename, :tag => tag, :gpgkey => self.gpgkey, :strict_keys => self.strict_keys)
         script.write_file
       end
     end
