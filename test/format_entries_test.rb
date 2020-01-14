d35debbb818bba7768018d6561ffa780de540733
James Jeffers <jdjeffers@gmail.com>
2020-01-14 15:31:56 -0500

    Skips features heading if there are no features.

---
+++ test/format_entries_test.rb
@@ -0,0 +1,49 @@
+require 'minitest/autorun'
+require 'minitest/mock'
+require 'tool_belt'
+require 'mocha/minitest'
+
+class FormatEntriesTest < MiniTest::Test
+
+  def setup
+    issue = Redmine::Issue.new
+    issue.raw_data = {
+      'issue' => {
+        'status' => {
+          'name'=> 'Closed'
+        },
+        'tracker' => {
+          'name' => "Feature"
+        }
+      }
+    }
+
+    @issues = [
+      issue
+      ]
+
+    @config = mock('release')
+    @config.stubs(:release).returns('v1.0')
+    @config.stubs(:code_name).returns('atest')
+
+    @env = mock('release_environment')
+    @env.stubs(:main_repo).returns('arepo')
+    @env.stubs(:repo_location).returns('someurl')
+
+    ToolBelt::Changelog.any_instance.stubs(:write_changelog)
+  end
+
+  def test_generates_features_heading_when_features_are_not_empty
+    changelog = ToolBelt::Changelog.new(@config, @env, @issues)
+    entries_string = changelog.format_entries
+
+    assert entries_string.include? "Features"
+  end
+
+  def test_skips_features_heading_when_features_is_empty
+    changelog = ToolBelt::Changelog.new(@config, @env, [])
+    entries_string = changelog.format_entries
+
+    refute entries_string.include? "Features"
+  end
+end
