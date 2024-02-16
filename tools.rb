c73d245aba4dcbb2d4e8c3951538e678105f687f
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-02-16 14:35:41 +0100

    Drop BranchDocsCommand
    Since Katello 4.0 the documentation lives in foreman-documentation so
    this is no longer needed.

--- tools.rb
+++ tools.rb
@@ -11,7 +11,6 @@ class MainCommand < Clamp::Command
   subcommand "cherry-picks", "Calculate needed cherry picks for a given release configuration", ToolBelt::Command::CherryPickCommand
   subcommand "changelog", "Generate changelog for a given release", ToolBelt::Command::ChangelogCommand
   subcommand "branch", "Create and push the branch or tag for each repository as specified in the configuration", ToolBelt::Command::BranchCommand
-  subcommand "branch-docs", "Generate docs for a given release", ToolBelt::Command::BranchDocsCommand
   subcommand "setup-environment", "Setup release environment for a given configuration", ToolBelt::Command::SetupEnvironmentCommand
   subcommand "check-deprecation-warnings", "Check codebase for outdated deprecation warnings", ToolBelt::Command::CheckDeprecationWarningsCommand
 end
