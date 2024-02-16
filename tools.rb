ae3c3d26640f1028c9fba015335fa0f36bb1c915
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-02-16 14:31:39 +0100

    Drop Koji related code
    Since Foreman 3.9 we no longer use Koji but instead use COPR. That means
    the tags and mash scripts code can be dropped.

--- tools.rb
+++ tools.rb
@@ -13,9 +13,6 @@ class MainCommand < Clamp::Command
   subcommand "branch", "Create and push the branch or tag for each repository as specified in the configuration", ToolBelt::Command::BranchCommand
   subcommand "branch-docs", "Generate docs for a given release", ToolBelt::Command::BranchDocsCommand
   subcommand "setup-environment", "Setup release environment for a given configuration", ToolBelt::Command::SetupEnvironmentCommand
-  subcommand "pulp-repo-update", "Update Katello's Pulp repository based on parameters", ToolBelt::Command::PulpRepositoryUpdateCommand
-  subcommand "koji", "Commands for various Koji release related tasks", ToolBelt::Command::KojiCommand
-  subcommand "mash-scripts", "Generate mash script files for a release", ToolBelt::Command::MashScriptsCommand
   subcommand "check-deprecation-warnings", "Check codebase for outdated deprecation warnings", ToolBelt::Command::CheckDeprecationWarningsCommand
 end
 
